1c893c3badc444d673a82a2b9305c9f67ca353f0
DavidDev25 <david.werner@david-werner.dev>
2025-03-18 00:52:52 +0100

    add navbar and main content components with basic structure and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaincontentComponent } from './maincontent/maincontent.component';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MaincontentComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'MainPortfolio';
