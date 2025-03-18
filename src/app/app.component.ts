37200b5b7912112da6d5da0f6560b3d4083cb498
DavidDev25 <david.werner@david-werner.dev>
2025-03-18 01:26:42 +0100

    added navbar with transparent design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaincontentComponent } from './maincontent/maincontent.component';
-import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
