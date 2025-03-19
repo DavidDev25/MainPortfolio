f8173a1eba5ef198830e32c47b7bddc436d63bad
DavidDev25 <david.werner@david-werner.dev>
2025-03-19 19:21:49 +0100

    feat: refactor hero component to simplify slider implementation and enhance styling

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,29 +1,8 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss'],
 })
-export class HeroComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    this.initSlider();
-  }
-
-  initSlider(): void {
-    const slider = document.querySelector('.slider') as HTMLDivElement;
-
-    if (!slider) {
-      console.error('Slider nicht gefunden');
-      return;
-    }
-
-    const slides = slider.children;
-    const originalLength = slides.length;
-
-    for (let i = 0; i < originalLength; i++) {
-      const clone = slides[i].cloneNode(true) as Node;
-      slider.appendChild(clone);
-    }
-  }
-}
+export class HeroComponent {}
